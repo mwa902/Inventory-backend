2f02294890a94684bd7733209898de3f9bdbf88d
Abid Rajpoot <abid.ali@biafotech.com>
2026-07-06 18:16:51 +0500

    removed backend folder

--- backend/models/category.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const categorySchema = new mongoose.Schema({
-    category_name : { type: string, required: true},
-    description : {type: string, required: true},
-    created_at : {type:Date, required: true},
-});
-
-export default mongoose.model("Category",categorySchema);
