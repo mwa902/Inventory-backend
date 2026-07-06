346793ce52961b6c12ae3a44b7659c221ac65f3d
mwa902 <ahmed.wahad213@gmail.com>
2026-07-06 18:14:01 +0500

    git models

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    product_name = {type: String, required: true},
-    product_description = {type: String, required: true},
-    purchase_price = {type: Number, required: true},
-    selling_price = {type: Number, required: true},
-    quantity = {type: Number, required: true},
-    image = {type: String, required: true},
-    status= {type: String, required: true},
-    category = {type: mongoose.Schema.Types.ObjectId, ref:"Category", required: true},
-    supplier = {type: mongoose.Schema.Types.ObjectId, ref:"Supplier", required: true},
+    product_name : {type: String, required: true},
+    product_description : {type: String, required: true},
+    purchase_price : {type: Number, required: true},
+    selling_price : {type: Number, required: true},
+    quantity : {type: Number, required: true},
+    image : {type: String, required: true},
+    status : {type: String, required: true},
+    category : {type: mongoose.Schema.Types.ObjectId, ref:"Category", required: true},
+    supplier : {type: mongoose.Schema.Types.ObjectId, ref:"Supplier", required: true},
 
 },
 {timestamps: true}
--- backend/models/supplier.js
+++ backend/models/supplier.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
-const suppilerSchema = new mongoose.Schema({
-   Name = {type: String, required: true},
-   company = {type: String, required: true},
-   phone_number = {type: String, required: true},
-   status = {type: String, required: true},
-},
-{timestamp: true}
+const suppilerSchema = new mongoose.Schema(
+  {
+    Name: { type: String, required: true },
+    company: { type: String, required: true },
+    phone_number: { type: String, required: true },
+    status: { type: String, required: true },
+  },
+  { timestamp: true },
 );
 
-export default mongoose.model("supplier",supplierSchema);
+export default mongoose.model("supplier", supplierSchema);
